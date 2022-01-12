ebbcf9c310bcc1e89c84a6076510b3340efb8c48
xxxx <squall0609.kanon@gmail.com>
2022-01-12 17:54:18 +0900

    データ途中

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,7 +20,7 @@
             :items="items"
             dense
             filled
-            label="Filled"
+            label="相手キャラを入力してください"
           ></v-autocomplete>
         </v-col>
       </v-col>
@@ -30,6 +30,10 @@
         <p v-if="value !== null">
           <!-- {{マリオ}}の場合、ジャンプ投げ空上は{{~65%}}、２段ジャンプ空上は{{~76%}}で確定します -->
           {{ value }}の場合、ジャンプ投げ空上は{{ jump() }}、２段ジャンプ空上は{{ jump2() }}で確定します
+          <br>
+          ポケスタ２台上の場合、ジャンプ投げ空上は{{ poke() }}、２段ジャンプ空上は{{ poke2() }}で確定します。
+          <br>
+          ポケスタ２台下/終点の場合、ジャンプ投げ空上は{{ end() }}、２段ジャンプ空上は{{ end() }}で確定します。
         </p>
         <p v-else>
           キャラを選択してください
@@ -64,7 +68,7 @@ export default {
       "ピカチュウ",
       "ルイージ",
       "ネス",
-      "キャプテンファルコン",
+      "キャプテン・ファルコン",
       "プリン",
       "ピーチ/デイジー",
       "クッパ",
@@ -101,7 +105,7 @@ export default {
       "むらびと",
       "ロックマン",
       "Wii Fit トレーナー",
-      "Wii Fir トレーナー　腹式呼吸",
+      "Wii Fir トレーナー腹式呼吸",
       "ロゼッタ＆チコ",
       "リトル・マック",
       "ゲッコウガ",
@@ -197,13 +201,144 @@ export default {
       console.log(this.value);
       if(this.value == "マリオ"){
         return "~65%"
+      } else if(this.value == "ドンキーコング"){
+        return "~84%"
+      } else if(this.value == "リンク"){
+        return "~72%"
+      } else if(this.value == "サムス/ダークサムス"){
+        return "~77%"
+      } else if(this.value == "ヨッシー"){
+        return "~64%"
+      } else if(this.value == "カービィ"){
+        return "~60%"
+      } else if(this.value == "フォックス"){
+        return "~54%"
+      } else if(this.value == "ピカチュウ"){
+        return "~53%"
+      } else if(this.value == "ルイージ"){
+        return "~65%"
+      } else if(this.value == "ネス"){
+        return "~64%"
+      } else if(this.value == "キャプテン・ファルコン"){
+        return "~78%"
+      } else if(this.value == "プリン"){
+        return "~58%"
+      } else if(this.value == "ピーチ/デイジー"){
+        return "~61%"
+      } else if(this.value == "クッパ"){
+        return "~90%"
+      } else if(this.value == "アイスクライマー"){
+        return "~60%"
+      } else if(this.value == "シーク"){
+        return "~60%"
+      } else if(this.value == "ゼルダ"){
+        return "~60%"
+      } else if(this.value == "ドクターマリオ"){
+        return "~65%"
+      } else if(this.value == "ピチュー"){
+        return "~53%"
+      } else if(this.value == "ファルコ"){
+        return "~55%"
+      } else if(this.value == "マルス/ルキナ"){
+        return "~66%"
+      } else if(this.value == "こどもリンク"){
+        return "~63%"
+      } else if(this.value == "ガノンドロフ"){
+        return "~84%"
+      } else if(this.value == "ミュウツー"){
+        return "~63%"
+      } else if(this.value == "ロイ/クロム"){
+        return "~68%"
+      } else if(this.value == "Mr.ゲーム＆ウォッチ"){
+        return "~53%"
+      } else if(this.value == "メタナイト"){
+        return "~57%"
+      } else if(this.value == "ピット/ブラックピット"){
+        return "~64%"
+      } else if(this.value == "ゼロスーツサムス"){
+        return "~65%"
+      } else if(this.value == "ワリオ"){
+        return "~69%"
+      } else if(this.value == "スネーク"){
+        return "~81%"
+      } else if(this.value == "アイク"){
+        return "~78%"
+      } else if(this.value == "ゼニガメ"){
+        return "~54%"
+      } else if(this.value == "フシギソウ"){
+        return "~63%"
+      } else if(this.value == "リザードン"){
+        return "~79%"
+      } else if(this.value == "ディディーコング"){
+        return "~61%"
+      } else if(this.value == "リュカ"){
+        return "~65%"
+      } else if(this.value == "ソニック"){
+        return "~61%"
+      } else if(this.value == "デデデ"){
+        return "~87%"
+      } else if(this.value == "ピクミン＆オリマー"){
+        return "~54%"
+      } else if(this.value == "ルカリオ"){
+        return "~71%"
+      } else if(this.value == "ロボット"){
+        return "~82%"
+      } else if(this.value == "トゥーンリンク"){
+        return "~59%"
+      } else if(this.value == "ウルフ"){
+        return "~66%"
+      } else if(this.value == "むらびと"){
+        return "~60%"
+      } else if(this.value == "ロックマン"){
+        return "~71%"
+      } else if(this.value == "Wii Fit トレーナー"){
+        return "~68%"
+      } else if(this.value == "Wii Fit トレーナー腹式呼吸"){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
+      } else if(this.value == ""){
+        return "~%"
       }
     },
     jump2:function(){
         if(this.value == "マリオ"){
         return "~76%"
+      }else if(this.value == "ドンキーコング"){
+        return "~97%"
       }
-    }
+    },
+    poke:function(){
+
+    },
   }
 };
 </script>
